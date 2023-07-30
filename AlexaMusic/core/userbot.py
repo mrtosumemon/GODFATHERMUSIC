cd102444324b43d461a8bc54c0651036d8a23b21
Legend Godfather <139125060+Legendtosu@users.noreply.github.com>
2023-07-30 15:27:42 +0530

    Update userbot.py

--- AlexaMusic/core/userbot.py
+++ AlexaMusic/core/userbot.py
@@ -59,7 +59,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Alexa_Help")
+                await self.one.join_chat("The_Incricible")
             except:
                 pass
             assistants.append(1)
@@ -85,7 +85,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Alexa_Help")
+                await self.two.join_chat("The_incricible")
             except:
                 pass
             assistants.append(2)
@@ -111,7 +111,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Alexa_Help")
+                await self.three.join_chat("The_Incricible")
             except:
                 pass
             assistants.append(3)
@@ -137,7 +137,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Alexa_Help")
+                await self.four.join_chat("The_incricible")
             except:
                 pass
             assistants.append(4)
@@ -163,7 +163,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Alexa_Help")
+                await self.five.join_chat("The_Incricible")
             except:
                 pass
             assistants.append(5)
