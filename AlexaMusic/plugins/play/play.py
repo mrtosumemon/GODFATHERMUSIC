382f11cfe38341fdb0b5978ae996ccf763abc933
Legend Godfather <139125060+Legendtosu@users.noreply.github.com>
2023-07-30 15:32:54 +0530

    Update play.py

--- AlexaMusic/plugins/play/play.py
+++ AlexaMusic/plugins/play/play.py
@@ -60,7 +60,7 @@ async def play_commnd(
 ):
     if not await is_served_user(message.from_user.id):
         await message.reply_text(
-            text="😢 ᴅᴇᴀʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀ ᴠᴇʀɪғɪᴇᴅ ᴀᴛ ᴀʟᴇxᴀ ᴅᴀᴛᴀʙᴀsᴇ.\n☔ ᴘʟᴇᴀsᴇ ᴜsᴇ /verify ᴛᴏ ᴠᴇʀɪғʏ ʏᴏᴜʀsᴇʟғ ᴀᴛ ᴀʟᴇxᴀ ᴅᴀᴛᴀʙᴀsᴇ.",
+            text="😢 ᴅᴇᴀʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀ ᴠᴇʀɪғɪᴇᴅ ᴀᴛ ɪɴᴄʀɪᴄɪʙʟᴇ ᴅᴀᴛᴀʙᴀsᴇ.\n☔ ᴘʟᴇᴀsᴇ ᴜsᴇ /verify ᴛᴏ ᴠᴇʀɪғʏ ʏᴏᴜʀsᴇʟғ ᴀᴛ ɪɴᴄʀɪᴄɪʙʟᴇ ᴅᴀᴛᴀʙᴀsᴇ.",
         )
         return
     mystic = await message.reply_text(
